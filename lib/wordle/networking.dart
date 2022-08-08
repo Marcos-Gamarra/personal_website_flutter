667c9a137bf6c309b76eeed8f50c744896e295e7
marcos-gamarra <marcosgamarralopez@gmail.com>
2022-08-08 12:16:16 -0400

    changed ip address of backend

--- lib/wordle/networking.dart
+++ lib/wordle/networking.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 Future<String> getTarget(http.Client client) async {
-  var response = await client.get(Uri.http("192.168.0.19:8080", "/get_target"));
+  var response = await client.get(Uri.https("181.127.68.241:443", "/get_target"));
   print(response.body);
   return response.body;
 }
