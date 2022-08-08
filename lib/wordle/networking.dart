8fa91dd3b875a137f1c74ffa1577b5db7798b2f4
marcos-gamarra <marcosgamarralopez@gmail.com>
2022-08-08 13:13:08 -0400

    Changed ip address for domain name

--- lib/wordle/networking.dart
+++ lib/wordle/networking.dart
@@ -1,13 +1,13 @@
 import 'package:http/http.dart' as http;
 
 Future<String> getTarget(http.Client client) async {
-  var response = await client.get(Uri.https("181.127.68.241:443", "/get_target"));
+  var response = await client.get(Uri.https("marcosrene.ga", "/get_target"));
   print(response.body);
   return response.body;
 }
 
 Future<bool> isValidGuess(http.Client client, String guess) async {
-  String host = "181.127.68.241:443";
+  String host = "marcosrene.ga";
   String path = "/is_valid_guess/$guess";
   var response = await client.get(Uri.http(host, path));
   if (response.body == "1") {
